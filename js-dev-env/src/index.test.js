660a0e45807dac865714bf3eba7627a416c76c15
Charles <charlchad@Charless-MacBook-Pro.local>
2017-12-21 17:44:01 +0000

    dom testing

--- js-dev-env/src/index.test.js
+++ js-dev-env/src/index.test.js
@@ -1,7 +1,23 @@
 import {expect} from 'chai';
+import jsdom from 'jsdom';
+import fs from 'fs';
 
 describe('Our first test', () => {
   it('should pass', () => {
     expect(true).to.equal(true);
+  });
+});
+
+describe('index.html', () => {
+  it('should say hello', (done) => {
+    const index = fs.readFileSync('./src/index.html', 'utf-8');
+    // eslint-disable-next-line prefer-arrow-callback
+    jsdom.env(index, function(err, window) { // eslint-disable-line func-names
+      const h1 = window.document.getElementByTagName('h1')[0];
+
+      done();
+      expect(h1.innerHTML).to.equal('Hello, world!');
+      window.close();
+    });
   });
 });
