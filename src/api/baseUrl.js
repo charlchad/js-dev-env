43c4c4dfc7261035ba93db412e353ecd67ddcc5f
Charles <charlchadd@gmail.com>
2017-12-24 09:20:09 +0000

    hard code data for test server

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,4 +1,5 @@
 export default function getBaseUrl() {
+  // return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
   return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://mysterious-dawn-16770.herokuapp.com/';
 }
 
