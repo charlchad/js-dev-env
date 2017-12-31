e014bb103f9edafc2bea0b4212bdd7e8de9f6a2d
Charles <charlchadd@gmail.com>
2017-12-31 01:34:22 +0000

    made index.test.js airbnb lint complient

--- src/index.test.js
+++ src/index.test.js
@@ -10,12 +10,12 @@ describe('Our first test', () => {
 
 describe('index.html', () => {
   it('should have h1 that says Users', (done) => {
-    const index = fs.readFileSync('./src/index.html', "utf-8");
-    jsdom.env(index, function(err, window) {
+    const index = fs.readFileSync('./src/index.html', 'utf-8');
+    jsdom.env(index, (err, window) => {
       const h1 = window.document.getElementsByTagName('h1')[0];
-      expect(h1.innerHTML).to.equal("Users");
+      expect(h1.innerHTML).to.equal('Users');
       done();
       window.close();
     });
-  })
-})
+  });
+});
