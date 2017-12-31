57e564a355af1cb2559512a4284974417bd73be1
Charles <charlchadd@gmail.com>
2017-12-31 01:42:21 +0000

    made index.js airbnb compliant

--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ import './index.css';
 import {getUsers, deleteUser} from './api/userApi';
 
 // Populate table of users via API call.
-getUsers().then(result => {
-  let usersBody = "";
+getUsers().then((result) => {
+  let usersBody = '';
 
-  result.forEach(user => {
-    usersBody+= `<tr>
-      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+  result.forEach((user) => {
+    usersBody += `<tr>
+      <td><a href='#' data-id='${user.id}' class='deleteUser'>Delete</a></td>
       <td>${user.id}</td>
       <td>${user.firstName}</td>
       <td>${user.lastName}</td>
       <td>${user.email}</td>
-      </tr>`
+      </tr>`;
   });
 
   global.document.getElementById('users').innerHTML = usersBody;
@@ -21,12 +21,13 @@ getUsers().then(result => {
   const deleteLinks = global.document.getElementsByClassName('deleteUser');
 
   // Must use array.from to create a real array from a DOM collection
-  // getElementsByClassname only returns an "array like" object
-  Array.from(deleteLinks, link => {
-    link.onclick = function(event) {
+  // getElementsByClassname only returns an 'array like' object
+  /* eslint-disable array-callback-return */
+  Array.from(deleteLinks, (link) => {
+    link.onclick = (event) => {
       const element = event.target;
       event.preventDefault();
-      deleteUser(element.attributes["data-id"].value);
+      deleteUser(element.attributes['data-id'].value);
       const row = element.parentNode.parentNode;
       row.parentNode.removeChild(row);
     };
