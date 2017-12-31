3ee7d851534573875472544ea974694269e5c3fe
Charles <charlchadd@gmail.com>
2017-12-31 00:24:25 +0000

    made generateMockData.js airbnb compartible

--- buildScripts/generateMockData.js
+++ buildScripts/generateMockData.js
@@ -6,18 +6,18 @@
 
 /* eslint-disable no-console */
 
-import jsf from 'json-schema-faker';
-import {schema} from './mockDataSchema';
 import fs from 'fs';
 import chalk from 'chalk';
+import jsf from 'json-schema-faker';
+import {schema} from './mockDataSchema';
 
 const json = JSON.stringify(jsf(schema));
 
-fs.writeFile("./src/api/db.json", json, function (err) {
+fs.writeFile('./src/api/db.json', json, (err) => {
   if (err) {
-    return console.log(chalk.red(err));
+    console.log(chalk.red(err));
   } else {
-    console.log(chalk.green('REMEMBER TO REFRESH THE APP IF YOU DON\'T SEE ANY DATA'));
-    console.log(chalk.green("Mock data generated."));
+    console.log(chalk.green('REMEMBER TO REFRESH THE APP IF YOU DON\'T SEE ANY DATA OR JUST WAIT A FEW SECONDS'));
+    console.log(chalk.green('Mock data generated.'));
   }
 });
